c6d233f66248960999c9be909621f1333f5e3b05
STYLESTHEKING <a.fonsecag@uniandes.edu.co>
2023-05-02 19:17:33 -0500

    corrección

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 import decrypter.Decrypter;
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<> lista = new ArrayList()<>;
         Scanner reqs = new Scanner(System.in);
         System.out.println("Que algoritmo quieres utilizar? (SHA256/SHA512)");
         String algorithm = reqs.nextLine();
@@ -11,6 +13,14 @@ public class Main {
 
         System.out.println("Cuántos threads vas a utilizar?");
         int threads = Integer.parseInt(reqs.nextLine());
+
+        System.out.println("Cuál código criptografico vas a utilizar?");
+        String c = reqs.nextLine();
+        lista.add(algorithm);
+        lista.add(salt);
+        lista.add(threads);
+        lista.add(c);
+        PasswordCrakcer pc = new PasswordCracker(lista);
 
     }
 }
--- src/PasswordCracker.java
+++ src/PasswordCracker.java
@@ -1,97 +1,102 @@
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashSearch {
-    private static final char[] CHARS = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-    private static final String SALT_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789";
-    
-    private static String hash(String algorithm, String text) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance(algorithm);
-        md.update(text.getBytes());
-        byte[] digest = md.digest();
-        StringBuilder sb = new StringBuilder();
-        for (byte b : digest) {
-            sb.append(String.format("%02x", b & 0xff));
-        }
-        return sb.toString();
+    private static final String ALGORITHM_SHA256 = "SHA-256";
+    private static final String ALGORITHM_SHA512 = "SHA-512";
+    private static final char[] CHARSET = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    private static int numThreads;
+    private static String algorithm;
+    private static String hash;
+    private static String salt;
+
+    private static volatile boolean found;
+    private static String password;
+    private static String saltFound;
+
+    public HashSearch(ArrayList<> lista) {
+        this.algorithm = lista.get(0);
+        this.hash = lista.get(1);
+        this.salt = lista.get(2);
+        this.numThreads = lista.get(3);
     }
-    
-    private static String generateSalt() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 2; i++) {
-            int index = (int) (Math.random() * SALT_CHARS.length());
-            sb.append(SALT_CHARS.charAt(index));
+
+
+    public void iniciar(){
+
+
+        if (numThreads < 1 || numThreads > 2) {
+            System.out.println("Number of threads must be 1 or 2.");
+            return;
         }
-        return sb.toString();
-    }
-    
-    private static void search(String algorithm, String hash, String salt, int numThreads) throws InterruptedException, NoSuchAlgorithmException {
-        boolean found = false;
+
         long startTime = System.currentTimeMillis();
-        
-        int numChars = CHARS.length;
-        int numPerThread = numChars / numThreads;
-        if (numChars % numThreads != 0) {
-            numPerThread++;
+
+        if (numThreads == 1) {
+            search(0, CHARSET.length, "");
+        } else {
+            Thread t1 = new Thread(() -> search(0, CHARSET.length / 2, ""));
+            Thread t2 = new Thread(() -> search(CHARSET.length / 2, CHARSET.length, ""));
+            t1.start();
+            t2.start();
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        
-        Thread[] threads = new Thread[numThreads];
-        for (int i = 0; i < numThreads; i++) {
-            final int startIndex = i * numPerThread;
-            final int endIndex = Math.min(startIndex + numPerThread, numChars);
-            Thread thread = new Thread(() -> {
-                for (int j = startIndex; j < endIndex && !found; j++) {
-                    String v = String.valueOf(CHARS[j]);
-                    try {
-                        String vHash = hash(algorithm, v + salt);
-                        if (vHash.equals(hash)) {
-                            System.out.println("Found: V = " + v + ", S = " + salt);
-                            found = true;
-                            break;
-                        }
-                    } catch (NoSuchAlgorithmException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            threads[i] = thread;
-            thread.start();
-        }
-        
-        for (Thread thread : threads) {
-            thread.join();
-        }
-        
+
         long endTime = System.currentTimeMillis();
-        System.out.println("Search time: " + (endTime - startTime) + "ms");
+
+        if (found) {
+            System.out.println("Password found: " + password + ", Salt found: " + saltFound);
+            System.out.println("Elapsed time: " + (endTime - startTime) + "ms");
+        } else {
+            System.out.println("Password not found.");
+        }
     }
-    
-    public static void main(String[] args) throws InterruptedException, NoSuchAlgorithmException {
-        if (args.length != 3 && args.length != 4) {
-            System.out.println("Usage: java HashSearch <algorithm> <hash> <salt> [<numThreads>]");
-            System.exit(1);
+
+    private static void search(int start, int end, String prefix) {
+        if (found) {
+            return;
         }
-        
-        String algorithm = args[0];
-        String hash = args[1];
-        String salt = args[2];
-        int numThreads = args.length > 3 ? Integer.parseInt(args[3]) : 1;
-        
-        if (!algorithm.equals("SHA256") && !algorithm.equals("SHA512")) {
-            System.out.println("Invalid algorithm");
-            System.exit(1);
+
+        if (prefix.length() == 7) {
+            String fullPassword = prefix + salt;
+            String fullHash = hash(fullPassword);
+            if (fullHash.equals(hash)) {
+                found = true;
+                password = prefix;
+                saltFound = salt;
+            }
+        } else {
+            for (int i = start; i < end; i++) {
+                search(0, CHARSET.length, prefix + CHARSET[i]);
+            }
         }
-        
-        if (hash.length() != 64 && hash.length() != 128) {
-            System.out.println("Invalid hash");
-            System.exit(1);
+    }
+
+    private static String hash(String input) {
+        try {
+            MessageDigest digest;
+            if (algorithm.equals(ALGORITHM_SHA256)) {
+                digest = MessageDigest.getInstance(ALGORITHM_SHA256);
+            } else if (algorithm.equals(ALGORITHM_SHA512)) {
+                digest = MessageDigest.getInstance(ALGORITHM_SHA512);
+            } else {
+                throw new RuntimeException("Unknown algorithm: " + algorithm);
+            }
+            byte[] hashBytes = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hashBuilder = new StringBuilder();
+            for (byte b : hashBytes) {
+                hashBuilder.append(String.format("%02x", b));
+            }
+            return hashBuilder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Algorithm not available: " + algorithm);
         }
-        
-        if (salt.length() != 2) {
-            System.out.println("Invalid salt");
-            System.exit(1);
-        }
-        
-        search(algorithm, hash, salt, numThreads);
     }
 }
